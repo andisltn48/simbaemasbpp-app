41883fce1b27dfbee9576cf0bc871bda00ff70d4
andisltn48 <andiaril186@gmail.com>
2024-11-19 13:52:51 +0800

    [hotfix] fix: update logo on login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,7 +41,7 @@
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand">
-              <img src="assets/img/logo.jpg" alt="logo" width="100" class="shadow-light rounded-circle">
+              <img src="assets/img/logo-1.jpg" alt="logo" width="100" class="shadow-light rounded-circle">
             </div>
 
             <div class="card card-primary">
